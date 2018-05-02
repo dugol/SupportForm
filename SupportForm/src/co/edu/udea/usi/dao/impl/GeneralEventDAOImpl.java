cec1fac78299ace2f987e0f3c0d0c68b976e91d6
rgr2228 <rgr2228@gmail.com>
2018-05-02 00:12:35 -0500

    Avances Dao impl

--- SupportForm/src/co/edu/udea/usi/dao/impl/GeneralEventDAOImpl.java
+++ SupportForm/src/co/edu/udea/usi/dao/impl/GeneralEventDAOImpl.java
@@ -1,8 +1,13 @@
 package co.edu.udea.usi.dao.impl;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import co.edu.udea.usi.dao.GeneralEventDAO;
@@ -18,8 +23,15 @@ public class GeneralEventDAOImpl extends HibernateDaoSupport implements GeneralE
 
 	@Override
 	public GeneralEvent createGeneralEvent(GeneralEvent generalEvent) throws UsiDaoException {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = null;
+		try{
+			session = this.getSessionFactory().getCurrentSession();
+			session.save(generalEvent);
+			
+		}catch(HibernateException e){
+			throw new UsiDaoException(e);
+		}
+		return generalEvent;
 	}
 
 	@Override
@@ -30,19 +42,38 @@ public class GeneralEventDAOImpl extends HibernateDaoSupport implements GeneralE
 
 	@Override
 	public List<GeneralEvent> findAllGeneralEvent() throws UsiDaoException {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = null;
+		List<GeneralEvent> generalEvents = null;
+		try{
+			generalEvents = new ArrayList<GeneralEvent>();
+			session = this.getSessionFactory().getCurrentSession();
+			Criteria criteria =session.createCriteria(GeneralEvent.class);
+			generalEvents = criteria.list();
+		}catch(HibernateException e){
+			throw new UsiDaoException(e);
+		}
+		return generalEvents;
 	}
 
 	@Override
 	public List<GeneralEvent> findByRequest(Request request) throws UsiDaoException {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = null;
+		List<GeneralEvent> generalEvents = null;
+		try{
+			generalEvents = new ArrayList<GeneralEvent>();
+			session = this.getSessionFactory().getCurrentSession();
+			Query query = session.createQuery("from generalevent where request=:request");
+			query.setParameter("request",request);
+			generalEvents = query.list();
+		}catch(HibernateException e){
+			throw new UsiDaoException(e);
+		}
+		return generalEvents;
 	}
 
 	@Override
 	public List<GeneralEvent> findByAdmin(String admin) throws UsiDaoException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
